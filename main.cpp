e9d5897f3f5960221d723b055e8cb7fb233c7368
Егорка-бар <kushnarenko.03@inbox.ru>
2021-06-05 00:06:17 +0300

    'fifthcommit'

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
     cin >> i >> j;
     cout << "chisla: " << i << "; " << j;
     cout << "\ni + j = " << i + j << '\n'
-         << "i - j = " << i - j << '\n';
+         << "i - j = " << i - j << '\n'
+         << "i * j = " << i * j << '\n';
     return 0;
 }
